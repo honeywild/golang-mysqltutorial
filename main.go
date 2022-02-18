89dd117d95e6c4b8b6448b6b5358c95006331478
honeywild <honeywild@sevenlab.xyz>
2022-02-18 17:26:25 +0300

    fixed: Create product table failed with error sql: database is closed

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ func dsn(dbName string) string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, hostname, dbName)
 }
 
-func main() {
+func dbConnection() (*sql.DB, error) {
 	db, err := sql.Open("mysql", dsn(""))
 	if err != nil {
 		log.Printf("Error %s when opening DB\n", err)
-		return
+		return nil, err
 	}
 	defer db.Close()
 
@@ -34,13 +34,13 @@ func main() {
 	res, err := db.ExecContext(ctx, "CREATE DATABASE IF NOT EXISTS "+dbname)
 	if err != nil {
 		log.Printf("Error %s when creating DB\n", err)
-		return
+		return nil, err
 	}
 
 	no, err := res.RowsAffected()
 	if err != nil {
 		log.Printf("Error %s when fetching rows", err)
-		return
+		return nil, err
 	}
 	log.Printf("rows affected %d\n", no)
 
@@ -49,9 +49,9 @@ func main() {
 	db, err = sql.Open("mysql", dsn(dbname))
 	if err != nil {
 		log.Printf("Error %s when opening DB", err)
-		return
+		return nil, err
 	}
-	defer db.Close()
+	//defer db.Close()
 
 	db.SetMaxOpenConns(20)
 	db.SetMaxIdleConns(20)
@@ -62,7 +62,46 @@ func main() {
 	err = db.PingContext(ctx)
 	if err != nil {
 		log.Printf("Errors %s pinging DB", err)
-		return
+		return nil, err
 	}
 	log.Printf("Connected to DB %s successfully\n", dbname)
+	return db, nil
+}
+
+func createProductTable(db *sql.DB) error {
+
+	query := `CREATE TABLE IF NOT EXISTS product(product_id int primary key auto_increment, product_name text,product_price int, created_at datetime default CURRENT_TIMESTAMP, updated_at datetime default CURRENT_TIMESTAMP)`
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	// exec a query
+	res, err := db.ExecContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when creating product table", err)
+		return err
+	}
+	// check a previous query using RowsAffected method
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when getting rows affected", err)
+		return err
+	}
+	log.Printf("Rows affected when creating table: %d", rows)
+	return nil
+}
+
+func main() {
+
+	db, err := dbConnection()
+	if err != nil {
+		log.Printf("Error %s when getting db connection", err)
+		return
+	}
+	defer db.Close()
+	log.Printf("Successfully connected to database")
+	err = createProductTable(db)
+	if err != nil {
+		log.Printf("Create product table failed with error %s", err)
+		return
+	}
+
 }
